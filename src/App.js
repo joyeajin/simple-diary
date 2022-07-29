3d5b2b9e16647c29653d4b35863633b2389cdd0c
joyeajin <dpwls31200@gmail.com>
2022-07-29 14:18:39 +0900

    6-3 리스트렌더링

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-import './App.css';
-import DiaryEditor from './DiaryEditor';
+import "./App.css";
+import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
+
+const dummyList = [
+  {
+    id: 1,
+    author: "예진",
+    content: "닝ㅎㄴㅇㅎ",
+    created_date: new Date().getTime(),
+    emotion: 2,
+  },
+  {
+    id: 2,
+    author: "코스트코",
+    content: "닝ㅎㄴㅇㅎ2",
+    created_date: new Date().getTime(),
+    emotion: 5,
+  },
+  {
+    id: 3,
+    author: "이마트",
+    content: "닝ㅎㄴㅇ3",
+    created_date: new Date().getTime(),
+    emotion: 1,
+  },
+];
 
 function App() {
   return (
     <div className="App">
       <DiaryEditor />
+      <DiaryList diaryList={dummyList} />
     </div>
   );
 }
---
+++ src/DiaryItem.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const DiaryItem = ({ author, content, emotion, created_date, id }) => {
+  return (
+    <div className="DiaryItem">
+      <div className="info">
+        <span className="author_info">
+          작성자:{author} | 감정점수:{emotion}
+        </span>
+        <br />
+        <span>날짜:{new Date(created_date).toLocaleString()}</span>
+      </div>
+      <div className="content">내용:{content}</div>
+    </div>
+  );
+};
+
+export default DiaryItem;
